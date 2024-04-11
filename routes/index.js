8ca80e7e80fdc7e9938c7c3e8692b5d261f1b8bf
lillian <keynesiane@gmail.com>
2024-04-10 17:43:26 -0700

    add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+// Require controller modules
+const message_controller = require('../controllers/messageController');
+const user_controller = require('../controllers/userController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', message_controller.index);
+
+/* routes for getting and posting users such as for creating users, 
+logging them in, and changing the admin and membership attributes */
+
+router.get('/sign-up', user_controller.user_create_get);
+
+router.post('/sign-up', user_controller.user_create_post);
+
+router.get('/log-in', user_controller.user_login_get);
+
+router.post('/log-in', user_controller.user_login_post);
+
+router.get('/secret', user_controller.user_secret_get);
+
+router.post('/secret', user_controller.user_secret_post);
+
+router.get('/admin', user_controller.user_admin_get);
+
+router.post('/admin', user_controller.user_admin_post);
 
 module.exports = router;
